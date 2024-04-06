a41f02ddb2023480779c75a950f112fa0c6290b3
Lexbrazilev <burtyt@mail.ru>
2024-04-06 11:50:57 +0300

    Добавьте файлы проекта.

---
+++ Investors/Investors.py
@@ -0,0 +1,13 @@
+﻿min_sum = int(input("Введите сумму минимальную сумму инвестиций "))
+money_m = int(input("Введите колличество долларов Майкла "))
+money_i = int(input("Введите колличество долларов Ивана "))
+if money_m >= min_sum and money_i >= min_sum:
+	print("2")
+elif money_m >= min_sum:
+	print("Mike")
+elif money_i >= min_sum:
+	print("Ivan")
+elif (money_m + money_i) >= min_sum:
+	print("1")
+elif money_m < min_sum and money_i < min_sum:
+	print("0")
